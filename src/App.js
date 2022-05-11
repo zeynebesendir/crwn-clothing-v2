ecf22a8f306d9b13ed70d1838a560a98dbb82d90
zeynebesendir <zeynebesendir@gmail.com>
2022-05-11 14:25:28 +0100

    created nested routes for shop/* and category page

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        {/* Add /* to render child routes */}
+        <Route path='shop/*' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
-<Route path='checkout' element={<Checkout />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
